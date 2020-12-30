8d9a064f4dea2bbfacf6ed52870ca249889965a8
rohan-rokade <officalyrohanrokade@gmail.com>
2020-12-30 19:31:43 +0530

    null123

--- sem702.py
+++ sem702.py
@@ -7,26 +7,13 @@ Created on Tue Dec 29 17:54:31 2020
 
 
 import streamlit as st
-import pandas as pd
-import numpy as np
-import pymongo
 
 
-dashboard_selectbox = st.sidebar.selectbox(
-    "DASHBOARD",
-    ("Home", "Mapping of Telecom Infrastructure"))
 
-if dashboard_selectbox=="Home":
-    st.title("SEM 7 Project,Mapping of Telecom Infrastructure in GIS application done by ROHAN ROKADE & MAAZ AMAAN SHAIKH")
-    
-if dashboard_selectbox=="Mapping of Telecom Infrastructure" :
-    
-   
-        st.success("You selected a Telecom INfrastructure")
+st.title("Rohan")
         
         
-            df1=pd.read_csv("https://raw.githubusercontent.com/Rohan-Rokade/networkdetector/master/308.csv"")
-            st.table(df1)
+        
            
             
     
