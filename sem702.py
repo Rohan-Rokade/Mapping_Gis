0f39f069b63e74edaeb4d1b09ba0f16b0153ceb4
rohan-rokade <officalyrohanrokade@gmail.com>
2021-01-01 19:31:57 +0530

    check

--- sem702.py
+++ sem702.py
@@ -1,11 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 29 17:54:31 2020
 
-@author: hp
-"""
-    import streamlit as st
-    st.title("Rohan")
+import streamlit as st
+st.title("Rohan")
         
         
         
