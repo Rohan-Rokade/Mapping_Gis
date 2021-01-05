253c8d79102942d3f5420979716d919d98cc4e52
Rohan-Rokade <66519435+Rohan-Rokade@users.noreply.github.com>
2021-01-05 10:18:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"rohan.rokade.24861999@gmail.com\"\n\
+email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
